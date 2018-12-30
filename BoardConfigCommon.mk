086d0b67c974507e07bebcbb4c4799089b98d5c3
Alexis <alexisr245@gmail.com>
2018-12-30 14:07:27 +0100

    feat: Dexpreopt

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -146,6 +146,16 @@ SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 TARGET_USES_HWC2 := true
 TARGET_USES_GRALLOC1 := true
 
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
+
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
 
