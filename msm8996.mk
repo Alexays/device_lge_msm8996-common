b8f721e340cf787d2b4be14ce605a36c776545a7
Alexis <alexisr245@gmail.com>
2019-01-02 10:35:44 +0100

    feat: wifi offload

--- msm8996.mk
+++ msm8996.mk
@@ -376,6 +376,7 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    android.hardware.wifi.offload@1.0-service \
     libwpa_client \
     hostapd \
     wlutil \
